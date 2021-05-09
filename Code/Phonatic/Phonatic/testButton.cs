acc5aef4701536b8657a6c00f25e03db79177fe1
Ahmed Gamal <agmghazi@hotmail.com>
2021-05-10 01:54:32 +0300

    create first add-ins in arcMap

---
+++ Code/Phonatic/Phonatic/testButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geometry;
+
+namespace Phonatic
+{
+    public class testButton : ESRI.ArcGIS.Desktop.AddIns.Tool
+    {
+        public testButton()
+        {
+
+        }
+
+        protected override void OnUpdate()
+        {
+
+        }
+
+        protected override void OnMouseUp(ESRI.ArcGIS.Desktop.AddIns.Tool.MouseEventArgs arg)
+        {
+            int x = arg.X;
+            int y = arg.Y;
+
+            IMxDocument mxDocument = (IMxDocument)ArcMap.Application.Document;
+            IPoint pPoint = (IPoint)mxDocument.ActivatedView.ScreenDisplay.DisplayTransformation.ToMapPoint(x, y);
+
+            MessageBox.Show("Mouse X: " + x + Environment.NewLine + "Y: " + y + "Map Point X: "+ pPoint.X + Environment.NewLine + "Y: " + pPoint.Y);
+
+
+        }
+    }
+
+}
